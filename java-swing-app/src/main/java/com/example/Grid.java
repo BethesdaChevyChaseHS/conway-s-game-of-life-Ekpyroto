a8bb26eeb899cb3e43363aa419079db60c4812c0
Ekpyroto <127448759+Ekpyroto@users.noreply.github.com>
2024-10-04 12:15:21 +0000

    turning in my work

--- java-swing-app/src/main/java/com/example/Grid.java
+++ java-swing-app/src/main/java/com/example/Grid.java
@@ -66,12 +66,67 @@ public class Grid extends JPanel implements ActionListener {
     //ALL YOUR CODE GOES HERE
     public void nextGeneration() {
         //1 Create a new temporary new array to store the values of the next generation
-
+            //int[][] nextGen = grid.clone();
+            int[][] nextGen= new int[rows][cols];
+            int sum = 0;
+            boolean topSafe;
+            boolean bottomSafe;
+            //boolean leftSafe;
+            //boolean rightSafe;
         //2 Visit every cell in the new temporary grid. Check the number of neighboring cells, and based on the rules determine whether the cell will be alive or dead.
         //Watch out for edge cases!
-
+            for(int row = 0; row<rows; row++){
+                for(int col = 0; col<cols; col++){
+                    topSafe = false;
+                    bottomSafe = false;
+                    //leftSafe = false;
+                    //rightSafe = false;
+                    sum = 0;
+                    if(row>0){
+                        topSafe=true;
+                        sum+=grid[row-1][col];
+                    }
+                    if(row<rows-1){
+                        bottomSafe=true;
+                        sum+=grid[row+1][col];
+                    }
+                    if(col>0){
+                        //leftSafe=true;
+                        sum+=grid[row][col-1];
+                        if(topSafe){
+                            sum+=grid[row-1][col-1];
+                        }
+                        if(bottomSafe){
+                            sum+=grid[row+1][col-1];
+                        }
+                    }
+                    if(col<cols-1){
+                        //rightSafe=true;
+                        sum+=grid[row][col+1];
+                        if(topSafe){
+                            sum+=grid[row-1][col+1];
+                        }
+                        if(bottomSafe){
+                            sum+=grid[row+1][col+1];
+                        }
+                    }
+                    if(sum<=1){
+                        nextGen[row][col]=0;
+                    }
+                    if(sum==2&&grid[row][col]==1){
+                        nextGen[row][col]=1;
+                    }
+                    if(sum==3){
+                        nextGen[row][col]=1;
+                    }
+                    if(sum>3){
+                        nextGen[row][col]=0;
+                    }
+                    
+                }
+            }
         //3 Copy the values of your temporary grid to the real grid
-
+            grid=nextGen.clone();
 
         //don't mess with this part
         repaint();
